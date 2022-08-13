8489cd190fd0642f538c329599669b53a872f165
Caique Gomes <caiqueag.14@gmail.com>
2022-08-12 22:04:04 -0300

    third commit

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
+const card = document.querySelector('section');
+
+const userInfo = {
+    avatar: document.getElementById('avatar'),
+    name: document.getElementById('name'),
+    bio: document.getElementById("bio"),
+    followers: document.getElementById("followers"),
+    following: document.getElementById("following"),
+    company: document.getElementById("company"),
+    location: document.getElementById("location")
+}
+
+const icons = {
+
+}
+
 let apiURL = 'https://api.github.com/users/'
 
+const showCard = () => {
+    card.style.display = 'inline-block';
+}
+
 const createUserURL = username => apiURL + username;
+
+const verifyCompanyAndLocation = (userInfo, user) => {
+    userInfo.company.style.display = user.company === null ? 'none' : 'list-item';
+    userInfo.location.style.display = user.location === null ? 'none' : 'list-item';
+}
 
 const returnUser = () => {
     const userURL = createUserURL(input.value);
@@ -10,12 +35,22 @@ const returnUser = () => {
     fetch(userURL)
         .then(response => response.json())
         .then(user => {
-            console.log(user);
+            verifyCompanyAndLocation(userInfo, user);
+            console.log(user.company, user.location)
+            userInfo.avatar.src = user.avatar_url;
+            userInfo.name.innerHTML = user.name;
+            userInfo.bio.innerHTML = user.bio;
+            userInfo.followers.innerHTML = `<i class="fa fa-user-group"></i> <br> ${user.followers} seguidores`;
+            userInfo.following.innerHTML = `<i class="fa fa-user-group"></i> <br> ${user.following} seguindo`;
+            userInfo.company.innerHTML = `<i class="fa fa-building"></i> <br> ${user.company}`;
+            userInfo.location.innerHTML = `<i class="fa fa-location-dot"></i> <br> ${user.location}`;
         })
 }
 
-const searchUser = () => {
+const showUser = () => {
     returnUser();
+    showCard()
 }
 
-button.addEventListener('click', searchUser);
+button.addEventListener('click', showUser);
+
